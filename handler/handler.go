d67e16ab2a96131b63e44eaab49aa8abac5fc2a2
chucky-1 <ds.martynchik@gmail.com>
2021-10-15 14:59:55 +0300

    15.10/3

--- handler/handler.go
+++ handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"CatsCrud/service"
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -22,58 +23,55 @@ func(h *CatHandler) GetAllCats(c echo.Context) error {
 	return c.JSON(http.StatusOK, allcats)
 }
 
-//func (h *CatHandler) CreateCats(c echo.Context) error {
-//
-//	// Получаем параметры json. Здесь, потому что repository не имеет доступа к echo
-//	jsonMap := make(map[string]interface{})
-//	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
-//	if err != nil {
-//		return err
-//	}
-//
-//	cat, err := h.src.CreateCatsServ(jsonMap)
-//	if err != nil {
-//		return err
-//	}
-//	return c.JSON(http.StatusCreated, cat)
-//}
-//
-//func (h *CatHandler) GetCat(c echo.Context) error {
-//	// Достаём ID
-//	id := c.Param("id")
-//
-//	cat, err := h.src.GetCatServ(id)
-//	if err != nil {
-//		return err
-//	}
-//	return c.JSON(http.StatusOK, cat)
-//}
-//
-//func (h *CatHandler) UpdateCat(c echo.Context) error {
-//	// Достаём ID
-//	id := c.Param("id")
-//
-//	jsonMap := make(map[string]interface{})
-//	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
-//	if err != nil {
-//		return err
-//	}
-//
-//	cat, err := h.src.UpdateCatServ(id, jsonMap)
-//	if err != nil {
-//		return err
-//	}
-//	return c.JSON(http.StatusOK, cat)
-//}
-//
-//func (h *CatHandler) DeleteCat(c echo.Context) error {
-//	// Достаём ID
-//	id := c.Param("id")
-//
-//	cat, err := h.src.DeleteCatServ(id)
-//	if err != nil {
-//		return err
-//	}
-//	return c.JSON(http.StatusOK, cat)
-//}
-//
+func (h *CatHandler) CreateCats(c echo.Context) error {
+	jsonMap := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+	if err != nil {
+		return err
+	}
+
+	cat, err := h.src.CreateCatsServ(jsonMap)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, cat)
+}
+
+func (h *CatHandler) GetCat(c echo.Context) error {
+	// Достаём ID
+	id := c.Param("id")
+
+	cat, err := h.src.GetCatServ(id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, cat)
+}
+
+func (h *CatHandler) UpdateCat(c echo.Context) error {
+	// Достаём ID
+	id := c.Param("id")
+
+	jsonMap := make(map[string]interface{})
+	err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+	if err != nil {
+		return err
+	}
+
+	cat, err := h.src.UpdateCatServ(id, jsonMap)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, cat)
+}
+
+func (h *CatHandler) DeleteCat(c echo.Context) error {
+	// Достаём ID
+	id := c.Param("id")
+
+	cat, err := h.src.DeleteCatServ(id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, cat)
+}
--- service/service.go
+++ service/service.go
@@ -17,22 +17,18 @@ func (s *CatService) GetAllCatsServ() ([]*models.Cats, error) {
 	return s.repository.GetAllCats()
 }
 
-//func (s *CatService) CreateCatsServ(jsonMap map[string]interface{}) (*models.Cats, error) {
-//	rep := CatService{}
-//	return rep.repository.CreateCats(jsonMap)
-//}
-//
-//func (s *CatService) GetCatServ(id string) (*models.Cats, error) {
-//	rep := CatService{}
-//	return rep.repository.GetCat(id)
-//}
-//
-//func (s *CatService) UpdateCatServ(id string, jsonMap map[string]interface{}) (*models.Cats, error) {
-//	rep := CatService{}
-//	return rep.repository.UpdateCat(id, jsonMap)
-//}
-//
-//func (s *CatService) DeleteCatServ(id string) (*models.Cats, error) {
-//	rep := CatService{}
-//	return rep.repository.DeleteCat(id)
-//}
+func (s *CatService) CreateCatsServ(jsonMap map[string]interface{}) (*models.Cats, error) {
+	return s.repository.CreateCats(jsonMap)
+}
+
+func (s *CatService) GetCatServ(id string) (*models.Cats, error) {
+	return s.repository.GetCat(id)
+}
+
+func (s *CatService) UpdateCatServ(id string, jsonMap map[string]interface{}) (*models.Cats, error) {
+	return s.repository.UpdateCat(id, jsonMap)
+}
+
+func (s *CatService) DeleteCatServ(id string) (*models.Cats, error) {
+	return s.repository.DeleteCat(id)
+}
